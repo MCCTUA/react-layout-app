141e998f62a6b67221107646e0d793f99ed6de88
MCCTUA <mcctua@gmail.com>
2024-04-13 08:06:07 +0700

    การส่ง props เป็น Object  และ รับค่ามา loop โดยใช้ map

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,54 @@
 import CartItems from "./CartItems";
+const products = [
+  {
+    imgUrl: "https://techxcite.com/products/1382/gallery/S21%20Ultra.jpg",
+    name: "Samsung Galaxy S21 Ultra (12GB + 256GB)",
+    price: 41900,
+    qty: 1,
+  },
+  {
+    imgUrl: "https://techxcite.com/products/1362/gallery/Mate30Pro_01.jpg",
+    name: "Huawei Mate 30 Pro",
+    price: 28900,
+    qty: 1,
+  },
+  {
+    imgUrl: "https://techxcite.com/products/1292/gallery/Untitled-2%20fix.jpg",
+    name: "Samsung Galaxy A70",
+    price: 15990,
+    qty: 1,
+  },
+  {
+    imgUrl: "https://techxcite.com/products/1279/gallery/r1.png",
+    name: "Xiaomi Redmi note 7",
+    price: 4999,
+    qty: 1,
+  },
+  {
+    imgUrl: "https://techxcite.com/products/1271/gallery/S10pC_04.jpg",
+    name: "Samsung Galaxy S10+ Ceramic [1TB]",
+    price: 55900,
+    qty: 1,
+  },
+];
 
 const Cart = () => {
   return (
-    <div>
-      <CartItems name="Banana" price="30" qty="2" />
-      <CartItems name="Bread" price="15" qty="2" />
+    <div className="container">
+      <div className="row">
+        {products.map((product, index) => {
+          return (
+            <div className="col-md-4 my-3" key={index}>
+              <CartItems
+                imgUrl={product.imgUrl}
+                name={product.name}
+                price={product.price}
+                qty={product.qty}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/pages/CartItems.jsx
+++ src/pages/CartItems.jsx
@@ -1,9 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 const CartItems = (props) => {
+  console.log(props);
   return (
-    <div className="card">
-      <p> Name : {props.name}</p>
-      <p> Price : {props.price}</p>
-      <p> Qty : {props.qty}</p>
+    <div classname="card">
+      <div className="card">
+        <img src={props.imgUrl} className="card-img-top" alt="" />
+        <div className="card-body">
+          <h5 className="card-title">{props.name}</h5>
+          <p className="card-text">{props.price} THB</p>
+          <p className="card-text">{props.qty} items</p>
+        </div>
+      </div>
     </div>
   );
 };
